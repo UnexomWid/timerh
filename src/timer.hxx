47f80805f298a5e20528d63490e34f3c597be39e
UnexomWid <uw@exom.dev>
2020-04-23 12:38:44 +0300

    [REFACTOR] File extensions

--- src/timer.hxx
+++ src/timer.hxx
@@ -2,7 +2,7 @@
  * TimerH (https://github.com/UnexomWid/timerh)
  *
  * This project is licensed under the MIT license.
- * Copyright (c) 2018-2019 UnexomWid (https://uw.exom.dev)
+ * Copyright (c) 2018-2020 UnexomWid (https://uw.exom.dev)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction, including
@@ -20,37 +20,37 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#ifndef TIMERH_TIMER_H
-#define TIMERH_TIMER_H
+#ifndef TIMERH_TIMER_HXX
+#define TIMERH_TIMER_HXX
 
 #include <chrono>
 #include <string>
-#include <cstdint>
 
 #define CHRONOMETER std::chrono::high_resolution_clock::time_point
+#define ULL unsigned long long
 
 // Gets the current time.
 CHRONOMETER time_now();
 
 // Gets the execution time in hours.
-uint64_t get_exec_time_h(CHRONOMETER start);
+ULL get_exec_time_h(CHRONOMETER start);
 // Gets the execution time in minutes.
-uint64_t get_exec_time_m(CHRONOMETER start);
+ULL get_exec_time_m(CHRONOMETER start);
 // Gets the execution time in seconds.
-uint64_t get_exec_time_s(CHRONOMETER start);
+ULL get_exec_time_s(CHRONOMETER start);
 // Gets the execution time in milliseconds.
-uint64_t get_exec_time_ms(CHRONOMETER start);
+ULL get_exec_time_ms(CHRONOMETER start);
 // Gets the execution time in microseconds.
-uint64_t get_exec_time_mis(CHRONOMETER start);
+ULL get_exec_time_mis(CHRONOMETER start);
 // Gets the execution time in nanoseconds.
-uint64_t get_exec_time_ns(CHRONOMETER start);
+ULL get_exec_time_ns(CHRONOMETER start);
 
 // Formats a number representing milliseconds as a string.
-std::string format_time_ms(uint64_t milliseconds);
+std::string format_time_ms(ULL milliseconds);
 // Formats a number representing microseconds as a string.
-std::string format_time_mis(uint64_t microseconds);
+std::string format_time_mis(ULL microseconds);
 // Formats a number representing nanoseconds as a string.
-std::string format_time_ns(uint64_t nanoseconds);
+std::string format_time_ns(ULL nanoseconds);
 
 // Gets the formatted execution time with the precision of a millisecond.
 std::string getf_exec_time_ms(CHRONOMETER start);
